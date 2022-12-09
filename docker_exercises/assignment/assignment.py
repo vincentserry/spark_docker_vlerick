4ab00f92a2c4efa244814dfd424973f6ff68979c
vincentserry <vincent.serry@edu.vlerick.com>
2022-12-09 13:34:58 +0000

    commit 1.0

---
+++ docker_exercises/assignment/assignment.py
@@ -0,0 +1,22 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+import os
+
+print(os.environ["AWS_SECRET_ACCESS_KEY"])
+print('='*80)
+
+
+BUCKET = "dmacademy-course-assets"
+KEY1 = "vlerick/pre-release.csv"
+KEY2 = "vlerick/after-release.csv"
+
+config = {
+    "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
+    "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+}
+conf = SparkConf().setAll(config.items())
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+# Read the CSV file from the S3 bucket
+pre_release = spark.read.csv(f"s3a://{BUCKET}/{KEY1}", header=True).toPandas()
+after_release = spark.read.csv(f"s3a://{BUCKET}/{KEY2}", header=True).toPanda()
